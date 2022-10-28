5ecee9b919f178add9b8a0dba677da6d0e1ff401
Gonzalo Noreña <>
2022-10-28 21:51:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import tools
+
+def main():
+  size = eval(input("Indique el tamaño de la lista: "))
+  inf = eval(input("el rango inferior de los valores de la lista: "))
+  sup = eval(input("el rango superior de los valores de la lista: "))
+
+  list = tools.createList(size,inf,sup)
+  print(list)
+
+
+main()
---
+++ tools.py
@@ -0,0 +1,28 @@
+import random
+# retorna la suma de los elementos de la lista
+def sumList(list):
+  sum = 0
+  for i in list:
+    sum += i
+  return sum
+
+#muestra el primer elemento de la lista
+def showFirstPlace(list):
+  return list[0]
+
+#muestra el último elemento de la lista
+def showLastPlace(list):
+  return list[-1]
+
+#suma un valor a toda la lista
+def sumValAllList(list,val):
+  for i in range(len(list)):
+    list[i] += val
+
+#dado un tamaño crear una lista con valores de un rango
+def createList(size, inf, sup):
+  list = []
+  for i in range(size):
+    list.append(random.randint(inf,sup))
+  return list
+    
