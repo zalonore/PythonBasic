dd1d5fb3feb1688021f735e58141e975d45d2ea1
Gonzalo <zalonore@gmail.com>
2022-10-29 19:27:45 +0000

    cambie la variable size por tam

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import tools
 
 def main():
-  size = eval(input("Indique el tamaño de la lista: "))
+  tam = eval(input("Indique el tamaño de la lista: "))
   inf = eval(input("el rango inferior de los valores de la lista: "))
   sup = eval(input("el rango superior de los valores de la lista: "))
 
-  list = tools.createList(size, inf, sup)
+  list = tools.createList(tam, inf, sup)
   print("la lista ha queado así: \n", list)
   print("la suma de los elementos da: \n", tools.sumList(list))
   input("digite ENTER para terminar ")
