40b3ff61f701b05da97f8f05d64c11bc164aab8e
GonzaloNorea <>
2022-10-29 11:38:58 +0000

    solucion conflicto

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tools
 
 def main():
-  size = eval(input("Indique el tamaño de la lista: "))
+  size = eval(input("Indiq el tamaño de la lista: "))
   inf = eval(input("el rango inferior de los valores de la lista: "))
-  sup = eval(input("el rango superior de los valores de la lista: "))
+  sup = eval(input("el rango super de los valores de la lista: "))
 
   list = tools.createList(size,inf,sup)
   print("la lista ha queado así: \n",list)
