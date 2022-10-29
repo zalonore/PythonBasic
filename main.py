371ad34aa31159c44afc1e7329f476ef87a2341e
Gonzalo <zalonore@gmail.com>
2022-10-29 19:24:32 +0000

    se corrige parámetro de sumList

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ def main():
   inf = eval(input("el rango inferior de los valores de la lista: "))
   sup = eval(input("el rango superior de los valores de la lista: "))
 
-  list = tools.createList(size,inf,sup)
-  print("la lista ha queado así: \n",list)
-  print("la suma de los elementos da: \n",tools.sumList())
+  list = tools.createList(size, inf, sup)
+  print("la lista ha queado así: \n", list)
+  print("la suma de los elementos da: \n", tools.sumList(list))
   input("digite ENTER para terminar ")
+
 
 main()
