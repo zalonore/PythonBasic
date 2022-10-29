104e618eb68dd48cbcc4d006dcf29421986e694a
zalonore <zalonore@gmail.com>
2022-10-29 09:50:42 -0500

    corrección tipográfica en los print de main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import tools
 
 def main():
-  size = eval(input("Indiq el tamaño de la lista: "))
+  size = eval(input("Indique el tamaño de la lista: "))
   inf = eval(input("el rango inferior de los valores de la lista: "))
-  sup = eval(input("el rango super de los valores de la lista: "))
+  sup = eval(input("el rango superior de los valores de la lista: "))
 
   list = tools.createList(size,inf,sup)
   print("la lista ha queado así: \n",list)
   print("la suma de los elementos da: \n",tools.sumList())
   input("digite ENTER para terminar ")
-
-  
-
 
 main()
